086323f0c2c511e70dad6db1108da9c7391d7689
PUGAZHENDHI V <126157273+PugazhendhiVelu@users.noreply.github.com>
2024-06-12 22:54:20 +0530

    Update script1.js

--- static/script1.js
+++ static/script1.js
@@ -1,1 +1,24 @@
+function toggleLogin() {
+    document.getElementById("login-toggle").style.backgroundColor = "#f38b2a";
+    document.getElementById("login-toggle").style.color = "#fff";
+    document.getElementById("signup-toggle").style.backgroundColor = "#fff";
+    document.getElementById("signup-toggle").style.color = "#222";
+    document.getElementById("signup-form").style.display = "none";
+    document.getElementById("login-form").style.display = "block";
+
+    // Hide flash messages
+    document.querySelector('.flash-messages').style.display = 'none';
+}
+
+function toggleSignup() {
+    document.getElementById("login-toggle").style.backgroundColor = "#fff";
+    document.getElementById("login-toggle").style.color = "#222";
+    document.getElementById("signup-toggle").style.backgroundColor = "#f38b2a";
+    document.getElementById("signup-toggle").style.color = "#fff";
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("signup-form").style.display = "block";
+
+    // Hide flash messages
+    document.querySelector('.flash-messages').style.display = 'none';
+}
 
